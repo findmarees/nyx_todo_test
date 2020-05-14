1b2b203171a416c5611646d40d6bf68621a6c71a
findmarees <61082116+findmarees@users.noreply.github.com>
2020-05-14 17:26:17 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://marees:marees2020@mars-v6pyi.mongodb.net/nyx_todo?retryWrites=true&w=majority',
-    jwtKey: '@&*(*&BCIW)my$Secret$Jwt$key&%*)'
-};
-
-
- 
